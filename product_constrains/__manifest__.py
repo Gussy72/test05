9f5db1d94c558838b4779999bcb0fce4d22d3479
Gustavo Sanchez S <47462543+Gussy72@users.noreply.github.com>
2021-12-29 18:17:44 -0600

    product_constrains

---
+++ product_constrains/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Restricciones en Productos',
+    'version' : '14.0',
+    'summary': 'Modifica restricciones al modelo de Productos',
+    'sequence': 30,
+    'description': """
+Restricciones a los Productos
+==================================
+- Elimina la restriccion para que el codigo de barras del producto sea única por compañía.
+
+    """,
+    'category' : 'Tools',
+    'website': 'http://appti.mx/',
+    'images' : [],
+    'author': 'appti',
+    'depends' : ['product'],
+    'data': [
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
