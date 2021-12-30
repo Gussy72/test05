5150bee7533469b157ee276aebf905defd4ab21f
Gustavo Sanchez S <47462543+Gussy72@users.noreply.github.com>
2021-12-29 18:10:32 -0600

    partner_addenda

---
+++ partner_addenda/models/res_partner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    x_gln = fields.Char(string='GLN', store=True, help='Global Location Number')
+    x_center = fields.Char(string='Center', store=True, help="Customer Store")
+    x_partner_code = fields.One2many('res.partner.supplierinfo', 'name')
+    x_product_info = fields.One2many('product.supplierinfo.sale', 'partner_name')
+    x_edi_identification = fields.Char(string="Identificador EDI", store=True, help="Identificador EDI del contacto")
+    x_CedisTienda = fields.Selection(string='Cedis Tienda', selection=[('ON', 'Cedis'), ('DQ', 'Tienda'), ],
+                                     required=False, store=True, help="Destino del producto")
