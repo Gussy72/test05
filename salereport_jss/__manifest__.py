5728117c4ed4fca4a51b24e3d5f2dc4da7dfaa31
Gustavo Sanchez S <47462543+Gussy72@users.noreply.github.com>
2021-12-29 18:22:52 -0600

    salereport_jss

---
+++ salereport_jss/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Reporte venta',
+    'category': '',
+    'summary': '',
+    'author': 'JSS',
+    'website': '',
+    'version': '1.0',
+    'description':"""
+Reporte venta tipo ticket
+""",
+    'images': [],
+    'depends': ['sale'],
+    'data': [
+        'report.xml',
+        'report_paperformat.xml',
+        'report_receipt.xml',
+        'sale_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
